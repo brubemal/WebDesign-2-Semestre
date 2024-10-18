2a8ac819988e1153a8b7dc1979bea5013618bd8b
brubemal <brubemal@gmail.com>
2024-10-18 09:01:05 -0300

    Delete ex8.js

--- ex8.js
+++
@@ -1,9 +0,0 @@
-let produto = {
-    "nome":"Banana",
-    "preco":"20",
-    "quantidade":"30",
-    calcularTotal : function(){
-        return this.preco * this.quantidade;
-    }
-}
-console.log(produto.calcularTotal());
