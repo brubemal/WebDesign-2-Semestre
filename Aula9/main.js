d9fdc4fc3e5462757aff092321036b046661acee
brubemal <brubemal@gmail.com>
2024-10-25 09:06:00 -0300

    Add files via upload

---
+++ Aula9/main.js
@@ -0,0 +1,10 @@
+document.querySelector('#ntitle').onclick = function(){
+    document.querySelector('#titulo').innerHTML = "novoTexto"
+}
+document.querySelector('#pcolor').onclick = function(){
+    document.querySelector('p').style.color = 'red'
+}
+document.querySelector('#mario').onclick = function(){
+    document.querySelector('#ad').play()
+}
+const botoes = document.querySelectorAll('#btnst'){}
